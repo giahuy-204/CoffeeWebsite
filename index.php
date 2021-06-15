8c9e5893f254e5c537ebc8f488f02b848bf0124a
TaiNgoDuc <tai.ngo190209@vnuk.edu.vn>
2021-06-15 15:52:45 +0700

    mobile menu and login form fixed

--- index.php
+++ index.php
@@ -70,6 +70,10 @@ if (isset($_POST['add'])){
 
 <img id="thumbnail" src="https://i.imgur.com/QHMQohI.jpg?1" width="100%">
     <div class="class_container">
+        <div class="nav-toggle" id="navToggle">
+            <img id="navClosed" class="navIcon" src="https://www.richardmiddleton.me/wp-content/themes/richardcodes/assets/img/hamburger.svg" alt="hamburger menu">
+            <img id="navOpen" class="navIcon hidden" src="https://www.richardmiddleton.me/wp-content/themes/richardcodes/assets/img/close.svg" alt="close hamburger">
+        </div>
         <nav>
             <ul>
                 <li>
@@ -107,5 +111,6 @@ if (isset($_POST['add'])){
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="main.js"></script>
 </body>
 </html>
