196e8ae63eb59af6ade539c6fc41be3381e2511f
giahuy-204 <giahuy.dng@gmail.com>
2021-06-16 14:59:34 +0700

    add stylesheet link

--- index.php
+++ index.php
@@ -50,6 +50,7 @@ if (isset($_POST['add'])){
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>Coffee Menu</title>
 
     <!-- Font Awesome -->
