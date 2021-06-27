05a2427bb8ff34e9da3028dbdb8b71acc625ae6e
Huy Dang <giahuy.dng@gmail.com>
2021-06-27 16:29:09 +0700

    hotfix menu

--- menu.php
+++ menu.php
@@ -42,6 +42,9 @@ if (isset($_POST['add'])){
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>Coffee Menu</title>
 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
@@ -76,10 +79,7 @@ if (isset($_POST['add'])){
                     <a href="Contactus.html"><i class="fa fa-fw fa-user"></i>Contact us</a>
                 </li>
                 <li>
-                    <a href="manage.php"><i class="fa fa-cloud-upload"></i> Manage Product</a>
-                </li>
-                <li>
-                    <a href="manage_order.php"><i class="fa fa-first-order"></i> Manage Order</a>
+                    <a href="manage.php"><i class="fa fa-cloud-upload" aria-hidden="true"></i>Manage Product</a>
                 </li>
             </ul>
         </nav>
@@ -110,5 +110,3 @@ if (isset($_POST['add'])){
 </div>
 </body>
 </html>
-
-
